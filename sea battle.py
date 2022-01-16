d267b2d7d5bd1588cc53dd03ce7c5110d8ee2ab4
DonVito <vitoss32@mail.ru>
2022-01-16 07:36:03 +0300

    Created classes Dot and Ship (2)

--- sea battle.py
+++ sea battle.py
@@ -11,6 +11,7 @@ class Dot:
         return f'x:{self.x} y:{self.y}'
 
 class Ship(Dot):
+    dots_ = None
     def __init__(self, length=None, x=None, y=None, orientation="", life=None):
         self.length = length
         super().__init__(x, y)
@@ -20,9 +21,20 @@ class Ship(Dot):
     def get_Ship(self):
         return f'Ship have length: {self.length} and his position ({self.x}, {self.y})'
 
+    def dots(self):
+        if self.orientation == "H":
+            dots_ = tuple((self.x + h, self.y) for h in range(0, self.length))
+        elif self.orientation == "V":
+            dots_ = tuple((self.x, self.y + h) for h in range(0, self.length))
+        return dots_
+
+class Board(Ship):
+    
+
 
 dot = Dot()
 dot.set_Dot(1, 2)
 print(dot.get_Dot())
-ship = Ship(10, 0, 10, "h", 10)
+ship = Ship(4, 0, 4, "H", 10)
 print(ship.get_Ship(), ship.get_Dot())
+print(*ship.dots())
