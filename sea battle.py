ba91b3bc1d0df1241a7f0af9cace5301812a5fe7
DonVito <vitoss32@mail.ru>
2022-01-16 19:33:29 +0300

    Переписал класс Dot, Ship. Дописал версию_1 генератора случайного расположения коробля

--- sea battle.py
+++ sea battle.py
@@ -1,48 +1,89 @@
+import random
+
 class Dot:
-    def __init__(self, x=None, y=None):
-        self.x = x
-        self.y = y
+    def __init__(self, x=None, y=None, val='.'):
+        self.x = y
+        self.y = x
+        self.val = val
+
+    def __eq__(self, other):
+        return True if self.dot == other.dot else False
 
     def set_Dot(self, x, y):
-        self.x = x
-        self.y = y
+        self.dot = [x, y]
 
     def get_Dot(self):
-        return f'x:{self.x} y:{self.y}'
+        return f'x:{self.x} y:{self.y} - val:{self.val}'
 
-class Ship(Dot):
-    dots_ = None
-    def __init__(self, length=None, x=None, y=None, orientation="", life=None):
+class Ship():
+    def __init__(self, length=None, x=None, y=None, orientation='', life=None):
         self.length = length
-        super().__init__(x, y)
+        self.ship = Dot(x, y)
         self.orientation = orientation
         self.life = life
 
     def get_Ship(self):
-        return f'Ship have length: {self.length} and his position ({self.x}, {self.y})'
+        return f'Ship have length: {self.length} and his position ({self.ship.dot[0]}, {self.ship.dot[1]})'
 
     def dots(self):
+        dots_ = []
         if self.orientation == "H":
-            dots_ = tuple((self.x + h, self.y) for h in range(0, self.length))
+            for h in range(self.length):
+                dots_.append(Dot(self.ship.x + h, self.ship.y, '+'))
+            # dots_.append(list(Dot(self.ship.x + h, self.ship.y, '+')) for h in range(0, self.length))
         elif self.orientation == "V":
-            dots_ = tuple((self.x, self.y + h) for h in range(0, self.length))
+            for v in range(0, self.length):
+                dots_.append(Dot(self.ship.x, self.ship.y + v, '+'))
+            # dots_ = Dot((self.ship.dot[0], self.ship.dot[1] + v, '+') for v in range(0, self.length))
         return dots_
 
-class Board(Ship):
-    def __init__(self, ships=[], hid=False, ships_life=0):
-        self.board_ = [[]]*36
+class Board():
+    def __init__(self, ships=Ship(), hid=False, ships_life=0):
+        self.board_ = [[Dot() for _ in range(6)] for _ in range(6)]
+        # self.board_ = [Dot() for _ in range(6)]
+        # for j in range(6):
+        #     self.board_[j] = [Dot() for _ in range(6)]
+        # self.board_ = [[Dot(0, 0)]*6 for _ in range(36)]
         self.ships = ships
         self.hid = hid
         self.ships_life = ships_life
 
     def add_ship(self):
-        ...
+        i = 6
+        while i > 0:
+            i -= 1
+            # генераторы случайных расположений
+            ornt = random.choice("HV")
+            x = random.randint(0, 5)
+            y = random.randint(0, 5)
+            # генераторы случайных расположений
+            length = self.ships.length
+            ship = Ship(length, x, y, ornt, length)
+            # сохраняем корабль на доску
+            for j in range(len(ship.dots())):
+                self.board_[ship.dots()[j].x - 1][ship.dots()[j].y - 1].val = ship.dots()[j].val
+                # print(ship.dots()[j].val, ship.dots()[j].x, ship.dots()[j].y)
+            # for i in range(0, length):
+            #     self.board_ = [Dot(x, y, '+')]
+
 
     def contour(self):
         ...
 
     def show(self):
-        ...
+        # print("   | 1 | 2 | 3 | 4 | 5 | 6 |")
+        # print("---+---+---+---+---+---+---+")
+        # for i in range(0, 6):
+        #     print(f' {i+1} ', end='|')
+        #     for j in range(0, 6):
+        #         print(f' {self.board_[j].dot[0]} ', end='|')
+        #     print('\n---+---+---+---+---+---+---+')
+        print("    1  2  3  4  5  6 ")
+        for i in range(0, 6):
+            print(f' {i + 1} ', end='')
+            for j in range(0, 6):
+                print(f' {self.board_[i][j].val} ', end='')
+            print()
 
     def out(self, dot):
         return False if 0 < dot.x < 6 and 0 < dot.y < 6 else True
@@ -78,7 +119,7 @@ class Game(Player):
     def random_board(self):
         ...
 
-    def greet(self):
+    def greet():
         ...
 
     def loop(self):
@@ -88,11 +129,20 @@ class Game(Player):
         ...
 
 
-dot = Dot()
-dot.set_Dot(1, 2)
-print(dot.get_Dot())
-ship = Ship(4, 0, 4, "H", 10)
-print(ship.get_Ship(), ship.get_Dot())
-print(*ship.dots())
-board = Board(ship)
-print(board.board_)
+board_test = [[] for _ in range(0, 6)]
+print(board_test)
+ship = Ship(3, 2, 3, "V", 3)
+
+#print(ship.get_Ship(), ship.get_Dot())
+#print(*ship.dots())
+board = Board(ship, True, 1)
+print(board.ships.ship.get_Dot())
+# print(board.board_)
+# board.show()
+board.show()
+board.add_ship()
+board.show()
+
+# сама игра начинатся здесь
+game = Game
+game.greet()
