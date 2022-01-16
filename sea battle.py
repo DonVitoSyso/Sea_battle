444d424a028a8dabd02a149f16492635a5b0cefc
DonVito <vitoss32@mail.ru>
2022-01-16 12:23:18 +0300

    Created classes Board, Player, Game - without metods

--- sea battle.py
+++ sea battle.py
@@ -29,7 +29,63 @@ class Ship(Dot):
         return dots_
 
 class Board(Ship):
-    
+    def __init__(self, ships=[], hid=False, ships_life=0):
+        self.board_ = [[]]*36
+        self.ships = ships
+        self.hid = hid
+        self.ships_life = ships_life
+
+    def add_ship(self):
+        ...
+
+    def contour(self):
+        ...
+
+    def show(self):
+        ...
+
+    def out(self, dot):
+        return False if 0 < dot.x < 6 and 0 < dot.y < 6 else True
+
+    def shot(self):
+        ...
+
+class Player(Board):
+    def __init__(self, board_player1, board_player2):
+        self.board_player1 = Ship(board_player1)
+        self.board_player2 = Ship(board_player2)
+
+    def ask(self):
+        ...
+
+    def move(self):
+        ...
+
+class user(Player):
+    def ask(self):
+        ...
+
+class AI(Player):
+    def ask(self):
+        ...
+
+class Game(Player):
+    player1 = user
+    board1 = Board()
+    player2 = AI
+    board2 = Board()
+
+    def random_board(self):
+        ...
+
+    def greet(self):
+        ...
+
+    def loop(self):
+        ...
+
+    def start(self):
+        ...
 
 
 dot = Dot()
@@ -38,3 +94,5 @@ print(dot.get_Dot())
 ship = Ship(4, 0, 4, "H", 10)
 print(ship.get_Ship(), ship.get_Dot())
 print(*ship.dots())
+board = Board(ship)
+print(board.board_)
