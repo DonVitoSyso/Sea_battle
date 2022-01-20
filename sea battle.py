0e121943c95d9d2602d6f764c5e780ed79126e1c
DonVito <vitoss32@mail.ru>
2022-01-20 06:39:52 +0300

    Привожу классы к соответствию. После просмотра вебинара. Дополнил класс AI, Player и частично Game

--- sea battle.py
+++ sea battle.py
@@ -1,5 +1,19 @@
 import random
 
+class BoardException(Exception):
+    ...
+
+class BoardOutException(BoardException):
+    def __str__(self):
+        return "Вы пытаетесь выстрелить за доску!"
+
+class BoardUsedException(BoardException):
+    def __str__(self):
+        return "Вы уже стреляли в эту клетку"
+
+class BoardWrongShipException(BoardException):
+    ...
+
 class Dot:
     def __init__(self, x=None, y=None, val='.'):
         self.x = x
@@ -22,20 +36,24 @@ class Ship():
     def get_Ship(self):
         return f'Ship have length: {self.length} and his position ({self.ship.dot[0]}, {self.ship.dot[1]})'
 
+    @property
     def dots(self):
         dots_ = []
         if self.orientation == "H":
             for h in range(self.length):
-                dots_.append(Dot(self.ship.x + h, self.ship.y, '+'))
+                dots_.append(Dot(self.ship_bow.x + h, self.ship_bow.y, '+'))
             # dots_.append(list(Dot(self.ship.x + h, self.ship.y, '+')) for h in range(0, self.length))
         elif self.orientation == "V":
-            for v in range(0, self.length):
-                dots_.append(Dot(self.ship.x, self.ship.y + v, '+'))
+            for v in range(self.length):
+                dots_.append(Dot(self.ship_bow.x, self.ship_bow.y + v, '+'))
             # dots_ = Dot((self.ship.dot[0], self.ship.dot[1] + v, '+') for v in range(0, self.length))
         return dots_
+    # эту часть еще не разбирал!!!!!!
+    # def shooten(self, shot):
+    #     return shot in self.dots
 
 class Board():
-    def __init__(self, hid=False, size = 6):
+    def __init__(self, hid=False, size=6):
         self.board_ = [["o" for _ in range(size)] for _ in range(size)]
         self.ships = []
         self.hid = hid
@@ -44,23 +62,23 @@ class Board():
         # этот тип непонятен
         self.busy = []
 
-    def add_ship(self, length=2):
-        i = 30
-        ship_tmp = Ship()
-        while i:
-            # генераторы случайных расположений
-            ornt = random.choice("HV")
-            x = random.randint(0, 5) if ornt == 'H' else random.randint(0, 5-length)
-            y = random.randint(0, 5-length) if ornt == 'H' else random.randint(0, 5)
-            # генераторы случайных расположений
-            ship = Ship(length, x, y, ornt)
-            # сохраняем корабль на доску
-            for j in range(len(ship.dots())):
-                if self.board_[ship.dots()[j].x][ship.dots()[j].y].val == '.':
-                    self.board_[ship.dots()[j].x][ship.dots()[j].y].val = ship.dots()[j].val
-                else:
-                    i -= 1
-                    break
+
+    def out(self, dot):
+        return False if 0 <= dot.x < 6 and 0 <= dot.y < 6 else True
+
+    def add_ship(self, ship):
+        # генераторы случайных расположений
+        ornt = random.choice("HV")
+        dot = Dot()
+        dot.x = random.randint(0, 5) if ornt == 'H' else random.randint(0, 5 - length)
+        dot.y = random.randint(0, 5 - length) if ornt == 'H' else random.randint(0, 5)
+        # генераторы случайных расположений
+        # сохраняеv корабль на доску
+        for i in ship.dots:
+            if self.out(i) or i in self.busy:
+                return BoardWrongShipException()
+            else:
+                ...
         return i
                 # print(ship.dots()[j].val, ship.dots()[j].x, ship.dots()[j].y)
 
@@ -82,9 +100,6 @@ class Board():
                 print(f' {self.board_[i][j].val} ', end='')
             print()
 
-    def out(self, dot):
-        return False if 0 <= dot.x < 6 and 0 <= dot.y < 6 else True
-
     def shot(self):
         ...
 
@@ -94,41 +109,84 @@ class Player(Board):
         self.board_player2 = Ship(board_player2)
 
     def ask(self):
-        ...
+        raise NotImplementedError()
 
     def move(self):
         ...
-
-class user(Player):
+# класс пользователь закончен - вводимые значения
+class User(Player):
     def ask(self):
-        ...
+        while True:
+            dot = input("Введите координаты: ").split()
+            if len(dot) < 2:
+                print("Введите 2 координаты")
+                continue
 
+            if not(dot[0].isdigit() and dot[1].isdigit()):
+                print("Введите числа")
+                continue
+
+            return Dot(int(dot[0]) - 1, int(dot[1]) - 1)
+# класс компьютера закончен - рандомные координаты
 class AI(Player):
     def ask(self):
-        ...
+        d = Dot(random.randint(0, 5), random.randint(0, 5))
+        print(f'Ход AI:({d.x}, {d.y})')
+        return d
 
+# класс Game осталось завести loop
 class Game(Player):
-    player1 = user
-    board1 = Board()
-    player2 = AI
-    board2 = Board()
-
     def random_board(self):
-        ...
+        ship_list = [3, 2, 2, 1, 1, 1, 1]
+        board = Board(size=self.size)
+        cont, i = 0, 0
+        while True:
+            cont += 1
+            if i == len(ship_list):
+                break
+            if cont > 2000:
+                i = 0
+            # генератор случайных расположений
+            ship = Ship(Dot(random.randint(0, self.size), random.randint(0, self.size)), ship_list[i],
+                        random.choice("HV"))
+            try:
+                board.add_ship(ship)
+                i += 1
+                continue
+            except BoardWrongShipException:
+                pass
+        board.begin()
+        return board
 
+
+    def __init__(self, size=6):
+        board1 = random_board()
+        board2 = random_board()
+        self.player1 = User(board1, board2)
+        self.player2 = AI(board2, board1)
+        # параметры для всех классов объявлены size, hid
+        self.size = size
+        self.hid = True
+
+    # greet закончен подсказка на игру
     def greet():
-        ...
+        print("-------------------")
+        print("  Приветсвуем вас  ")
+        print(" формат ввода: x y ")
 
     def loop(self):
         ...
-
+    # метод start описан
     def start(self):
-        ...
+        self.greet()
+        self.loop()
 
 
 board_test = [[] for _ in range(0, 6)]
 print(board_test)
-ship = Ship(3, 2, 3, "V")
+print(len(board_test))
+dot_ = Dot(2, 3)
+ship = Ship(3, dot_, "V")
 
 #print(ship.get_Ship(), ship.get_Dot())
 #print(*ship.dots())
@@ -144,16 +202,8 @@ board.show()
 
 list_1 = [Dot(y, x) for x, y in zip(range(4), range(4))]
 print(list_1)
-for i in range(4):
-    for j in range(4):
-        ...
-        # print(list_1[i][j].x, list_1[i][j].y, list_1[i][j].val)
+
 list_2 = [Dot(0,0), Dot(1,1), Dot(1,2)]
-# for x in list_2:
-#     for y in list_1:
-#         for z in y:
-#             if x == z:
-#                 print("Проверка прошла!", y)
 
 # print(list(filter(lambda i: i in list_2, list_2)))
 if all(x in list_1 for x in list_2):
